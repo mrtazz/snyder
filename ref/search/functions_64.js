9c1d18573a1cce59d8137a343e9986d17aac432b
Daniel Schauenberg <d@unwiredcouch.com>
2015-10-28 19:20:00 +0000

    rebuild pages at 0.4.0

---
+++ ref/search/functions_64.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['decrement',['Decrement',['../class_snyder_1_1_metrics_registry.html#a1a2224c3480757a288922e9f87158d5a',1,'Snyder::MetricsRegistry::Decrement(const std::string &amp;name)'],['../class_snyder_1_1_metrics_registry.html#a8e4dd4ce52a2fd92c838448e6ffe2ecf',1,'Snyder::MetricsRegistry::Decrement(const std::string &amp;name, uint64_t count)']]]
+];
--- ref/search/functions_69.js
+++ ref/search/functions_69.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['increment',['Increment',['../class_snyder_1_1_metrics_registry.html#a3a5a4dbb3cf5131c4c9a1a672a3ca843',1,'Snyder::MetricsRegistry::Increment(const std::string &amp;name)'],['../class_snyder_1_1_metrics_registry.html#a5a3180a4904b136f6700a21ab46acf64',1,'Snyder::MetricsRegistry::Increment(const std::string &amp;name, uint64_t count)']]]
+  ['increment',['Increment',['../class_snyder_1_1_metrics_registry.html#a65c9b1570d84b3add14bf2daa532ff53',1,'Snyder::MetricsRegistry::Increment(const std::string &amp;name)'],['../class_snyder_1_1_metrics_registry.html#aecbb82547a834a90b96a445e74fb3112',1,'Snyder::MetricsRegistry::Increment(const std::string &amp;name, uint64_t count)']]]
 ];
